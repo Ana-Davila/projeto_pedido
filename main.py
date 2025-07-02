88bf91b818c39b40856c5350b414d4256ea7caf3
Ana Davila Kulek <anadavila.kulek@icloud.com>
2025-07-02 20:42:09 -0300

    atualização do código

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#mensagem de boas-vindas
 print('------ Bem-vindo a Loja de Marmitas da Dona Ana Davila Kulek -----------')
 print('---------------------------Cardápio-------------------------------------')
 print('------------------------------------------------------------------------')
@@ -6,8 +7,24 @@ print('-----|    P     |        R$16.00          |         R$15.00       |-----'
 print('-----|    M     |        R$18.00          |         R$17.00       |-----')
 print('-----|    G     |        R$22.00          |         R$21.00       |-----')
 print('------------------------------------------------------------------------')
+acumulador = 0 #variável que acumula-rá o total dos pedidos
 
-while( sabor != 'BA' and sabor != 'FF'):
-    sabor = input('Entre com o sabor desejado (BA/FF): ')
-    print('Sabor inválido. Tente novamente.')
-    continue
+while True:
+    while True: # sabor com teste de validação
+        sabor = input('Entre com o pedido (BA/FF): ').upper()
+        if (sabor== 'BA' or sabor == 'FF'):
+            break
+        else:
+            print('Sabor inválido. Tente novamente. ')
+            continue
+    while True:
+        tamanho = ('Entre com o tamanho escolhido (P/M/G): ')
+        if(tamanho == 'P' or tamanho == 'M' or tamanho =='G'):
+            break
+        else:
+            print('Tamanho inválido. Tente novamente: ')
+            continue
+    break
+
+        
+
